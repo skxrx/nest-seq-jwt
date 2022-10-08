7942b72a6a8c28462201c5a8c9955542640e6a24
beliarsketcher <beliarsketcher@gmail.com>
2022-10-08 23:19:28 +0300

    Add modules to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function start() {
   const PORT = process.env.PORT || 3000
 
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Sequelize JWT Roles')
+    .setDescription('REST API documentation')
+    .setVersion('1.0.0')
+    .addTag('skxrx')
+    .build()
+  const documentation = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('/api/docs', app, documentation)
+
   await app.listen(PORT, async () => {
     console.log(`Server running on port ${PORT}`)
   })
